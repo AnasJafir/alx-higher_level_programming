02024a791a017fafaba210db10641b6e263f323a
AnasJafir <dr.anas.jafir@gmail.com>
2023-09-26 12:17:27 -0700

    correct the requirement fail of the second task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Definition of a square class """
+
+
 class Square:
     """ Defines a square
     Attributes:
-    _size (int): length of one side of the square
+        __size (int): length of one side of the square
     """
     def __init__(self, size):
         """initialization of the square
         Args:
-        size (int): length of one side of the square
+            size (int): length of one side of the square
         Returns: None
         """
         self.__size = size
