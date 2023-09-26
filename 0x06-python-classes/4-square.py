983be24d7b3d2db197c9b8d40a57aa22eca7376d
AnasJafir <dr.anas.jafir@gmail.com>
2023-09-26 12:27:35 -0700

    correct the code fail of the 5th task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,22 +5,21 @@
 class Square:
     """ Defines a square
     Attributes:
-    __size (int): length of one side of the square
+        __size (int): length of one side of the square
     """
     def __init__(self, size=0):
         """initialization of the square
         Args:
-        size (int): length of one side of the square
+            size (int): length of one side of the square
         Returns: None
         """
         self.size = size
 
     def area(self):
         """ Defines the current square area
-        Returns: 
-        the current square area
+        Returns: the current square area
         """
-        return self.__size ** 2
+        return (self.__size) ** 2
     @property
     def size(self):
         """to retrieve the size of the square
@@ -31,7 +30,7 @@ class Square:
     def size(self, value):
         """set the size
         Args:
-        value (int): the size of the square
+            value (int): the size of the square
         Returns: None
         """
         if type(value) is not int:
