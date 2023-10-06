656d345d176b234853bf319aa6922fc678ba9b1c
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-06 16:21:23 -0700

    creating a function that divide the element of a matrix by a number

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module containing a function that divides all element of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divide element of matrix number by number.
+
+    matrix: matrix to be divided.
+    div: number to divide the matrix.
+
+    Return: A list results.
+    """
+    new_list = []
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for i in matrix:
+        lst = []
+        if type(i) is not list:
+            raise TypeError("matrix must be a matrix\
+ (list of lists) of integers/floats")
+        if(len(matrix[0]) != len(i)):
+            raise TypeError("Each row of the matrix\
+ must have the same size")
+        for j in range(len(i)):
+            if (type(i[j]) is not int) and (type(i[j]) is not float):
+                raise TypeError("matrix must be a matrix\
+(list of lists) of integers/floats")
+            lst.append(round(((i[j]) / div), 2))
+        new_list.append(lst)
+    return new_list
