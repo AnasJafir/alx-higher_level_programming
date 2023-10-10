3554bfda3c7260bf8c79fddb491ca0daee0940bd
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-10 13:50:49 -0700

    function that reads a text file and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ reads a file an prints it to stdout
 def read_file(filename=""):
     """Function that reads a file and prints it"""
     with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end='')
