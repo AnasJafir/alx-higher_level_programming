6d63880f55212ebe93d270108e43ea5f5a1b6887
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-10 14:00:57 -0700

    function that reads a text file and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-"""Module containing a function that
-reads a file an prints it to stdout
-"""
-
-
 def read_file(filename=""):
-    """Function that reads a file and prints it"""
-    try:
-        with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
-            print(f.read(), end='')
-    except FileNotFoundError:
-        print("file doesn't exist")
-    except PermissionError:
-        print("file permission")
+    with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
+        print(f.read(), end='')
