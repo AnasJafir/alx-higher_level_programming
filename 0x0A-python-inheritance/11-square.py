b14580020672807bb5b1c718d945af670c1f7d88
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-11 16:16:15 -0700

    Fix: documentation of module

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -21,4 +21,4 @@ class Square(Rectangle):
     def __str__(self):
         """function to return square description"""
 
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__width, self.__height)
