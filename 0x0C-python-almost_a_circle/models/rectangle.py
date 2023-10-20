d693404d914181ce13c694e2e8775ad08efcb9bb
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-20 14:02:50 -0700

    adding area+display+update methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,7 +73,8 @@ class Rectangle(Base):
 
     def display(self):
         """Print Rectangle of #"""
-        s = ('#' * self.width + '\n') * self.height
+        s = '\n' * self.y + (' ' * self.x + '#' * self.width + '\n')\
+            * self.height
         print(s, end='')
 
     def __str__(self):
@@ -82,6 +83,19 @@ class Rectangle(Base):
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
         return infos
+
+    def update_class(self, id=None, width=None, height=None, x=None, y=None):
+        """Update Class Rectangle"""
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
     def update_class(self, id=None, width=None, height=None, x=None, y=None):
         """Update Class Rectangle"""
