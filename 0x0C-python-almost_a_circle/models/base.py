c8b237a9743df2395b1bae2dde978560559609c9
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-20 14:52:29 -0700

    Defining base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,9 +15,10 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ convert data to JSON """
-        if list_dictionaries is None or list_dictionaries == "[]":
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
