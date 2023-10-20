17e53b789102e83d0792e4e3a21b674d0b18aee6
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-20 12:10:56 -0700

    Create Python Package and a class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for Base Class"""
+
+
+class Base:
+    """Class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
