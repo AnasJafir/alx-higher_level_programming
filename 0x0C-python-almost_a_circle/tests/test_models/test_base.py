e883fcf70ed783e4c37b1cd88c4d440a2c7cb797
AnasJafir <dr.anas.jafir@gmail.com>
2023-10-20 15:23:27 -0700

    uni test the classes and methods

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,78 +1,93 @@
 #!/usr/bin/python3
+"""Module for test Base class"""
 import unittest
-from unittest.mock import patch
-from io import StringIO
-import os
 from models.base import Base
 from models.square import Square
 from models.rectangle import Rectangle
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+
 
 class TestBaseMethods(unittest.TestCase):
+    """Suite to test Base class"""
+
     def setUp(self):
+        """Method invoked for each test"""
         Base._Base__nb_objects = 0
 
-    def test_create_base_with_id(self):
+    def test_id(self):
+        """Test assigned id"""
         new = Base(1)
         self.assertEqual(new.id, 1)
 
-    def test_create_base_with_default_id(self):
+    def test_id_default(self):
+        """Test default id"""
         new = Base()
         self.assertEqual(new.id, 1)
 
-    def test_increment_id_across_instances(self):
-        new1 = Base()
+    def test_id_nb_objects(self):
+        """Test nb object attribute"""
+        new = Base()
         new2 = Base()
         new3 = Base()
-        self.assertEqual(new1.id, 1)
+        self.assertEqual(new.id, 1)
         self.assertEqual(new2.id, 2)
         self.assertEqual(new3.id, 3)
 
-    def test_mix_assigned_and_default_ids(self):
-        new1 = Base()
+    def test_id_mix(self):
+        """Test nb object attributes and assigned id"""
+        new = Base()
         new2 = Base(1024)
         new3 = Base()
-        self.assertEqual(new1.id, 1)
+        self.assertEqual(new.id, 1)
         self.assertEqual(new2.id, 1024)
         self.assertEqual(new3.id, 2)
 
-    def test_create_base_with_string_id(self):
+    def test_string_id(self):
+        """Test string id"""
         new = Base('1')
         self.assertEqual(new.id, '1')
 
-    def test_create_base_with_additional_arguments(self):
+    def test_more_args_id(self):
+        """Test passing more args to init method"""
         with self.assertRaises(TypeError):
-            Base(1, 1)
+            new = Base(1, 1)
 
-    def test_access_private_attribute(self):
+    def test_access_private_attrs(self):
+        """Test accessing to private attributes"""
         new = Base()
         with self.assertRaises(AttributeError):
             new.__nb_objects
 
-    def test_save_to_file_with_None(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            Rectangle.save_to_file(None)
-            self.assertEqual(mock_stdout.getvalue(), '[]\n')
-
-    def test_save_to_file_with_empty_list(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            Rectangle.save_to_file([])
-            self.assertEqual(mock_stdout.getvalue(), '[]\n')
-
-    def test_save_to_file_square(self):
+    def test_save_to_file_1(self):
+        """Test JSON file"""
         Square.save_to_file(None)
+        res = "[]\n"
         with open("Square.json", "r") as file:
-            self.assertEqual(file.read(), '[]\n')
+            with patch('sys.stdout', new=StringIO()) as str_out:
+                print(file.read())
+                self.assertEqual(str_out.getvalue(), res)
         try:
             os.remove("Square.json")
-        except FileNotFoundError:
+        except:
             pass
+        Square.save_to_file([])
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
 
-    def test_save_to_file_rectangle(self):
+    def test_save_to_file_2(self):
+        """Test JSON file"""
         Rectangle.save_to_file(None)
+        res = "[]\n"
         with open("Rectangle.json", "r") as file:
-            self.assertEqual(file.read(), '[]\n')
+            with patch('sys.stdout', new=StringIO()) as str_out:
+                print(file.read())
+                self.assertEqual(str_out.getvalue(), res)
         try:
             os.remove("Rectangle.json")
-        except FileNotFoundError:
+        except:
             pass
-
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
