1aca828943f8083cde054f4317c96d43ce0583cc
AnasJafir <dr.anas.jafir@gmail.com>
2023-12-11 15:28:36 -0800

    Calculate factorial of argument

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+const arg = process.argv.slice(2);
+const num = parseInt(arg[0]);
+const result = factorial(num);
+console.log(result);
