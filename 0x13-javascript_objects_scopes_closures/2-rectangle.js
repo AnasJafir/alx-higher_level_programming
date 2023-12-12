1426bc6c6b0dbc4503446e44498062d4758b78d7
AnasJafir <dr.anas.jafir@gmail.com>
2023-12-12 23:09:01 +0100

    initializing positive values to attributes

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return this.constructor.name;
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
