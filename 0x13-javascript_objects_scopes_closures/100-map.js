24de8e8443830d93fb2cc3d2244b0a83d126906e
AnasJafir <dr.anas.jafir@gmail.com>
2023-12-13 14:51:30 +0100

    import an array an return a modified copy of it

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const { list } = require('./100-data');
-const newArr = list.map((value, index) => value * index);
 console.log(list);
-console.log(newArr);
+console.log(list.map((value, index) => value * index));
