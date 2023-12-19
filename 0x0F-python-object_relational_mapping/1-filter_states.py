20d98cfdf00de261b4d278d0199728feb0ac2b42
AnasJafir <dr.anas.jafir@gmail.com>
2023-12-19 11:44:01 -0800

    listing states that begin with a specific letter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List all states from a database"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cursor = connection.cursor()
+    query = "SELECT * FROM states WHERE UPPER(name) LIKE 'N%' ORDER BY id;"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
