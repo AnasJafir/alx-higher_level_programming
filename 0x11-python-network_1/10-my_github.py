74ace2b3baee821cb745aae338c87a02c4032198
AnasJafir <dr.anas.jafir@gmail.com>
2024-01-09 10:22:50 -0800

    Github id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,16 +5,13 @@ and display id using Github API
 """
 
 import requests
+from requests.auth import HTTPBasicAuth
 import sys
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    url = f"https://api.github.com/users/{username}"
-    response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data["id"]
-        print(user_id)
-    else:
-        print("None")
+    url = "https://api.github.com/users/{}".format(username)
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    user_id = response.json().get('id')
+    print(user_id)
