53ffa49b30b5a65cbf302914bf4ae5bf61ac9fed
AnasJafir <dr.anas.jafir@gmail.com>
2024-01-09 08:28:11 -0800

    Fetche an URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that fetches an URL"""
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("    - type:", type(body))
+    print("    - content:", body)
+    print("    - utf8 content:", body.decode('utf-8'))
