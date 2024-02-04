474be3d7b901083c312f6561b8e80db4c432d6a3
AnasJafir <132464096+AnasJafir@users.noreply.github.com>
2024-02-04 15:08:50 +0100

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/102-top_city.sql
+++
@@ -1,7 +0,0 @@
--- Import in hbtn_0c_0 database a table
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-WHERE `month` = 7 OR `month` = 8
-GROUP BY `city`
-ORDER BY `avg_temp` DESC
-LIMIT 3;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++
@@ -1,2 +0,0 @@
--- Script that removes a database
-DROP DATABASE IF EXISTS `hbtn_0c_0`;
