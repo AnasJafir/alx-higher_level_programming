a11f7588e1e499aeb228f88eb24a10c650cb7bd8
AnasJafir <132464096+AnasJafir@users.noreply.github.com>
2024-02-04 15:09:47 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv.slice(2).map(Number);
-const numArg = arg.length;
-if (numArg <= 1) {
-  console.log(0);
-} else {
-  const sortedArg = Array.from(new Set(arg)).sort((a, b) => b - a);
-  console.log(sortedArg[1]);
-}
