efb169d37bbc463c52162be72819b4d7c3ebbcd1
AnasJafir <132464096+AnasJafir@users.noreply.github.com>
2024-02-04 15:10:21 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const { list } = require('./100-data');
-const newArr = list.map((value, index) => value * index);
-console.log(list);
-console.log(newArr);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const fileA = process.argv[2];
-const fileB = process.argv[3];
-const fileC = process.argv[4];
-const content1 = fs.readFileSync(fileA, { encoding: 'utf8' });
-const content2 = fs.readFileSync(fileB, { encoding: 'utf8' });
-const result = `${content1}${content2}`;
-fs.writeFileSync(fileC, result, { encoding: 'utf8' });
