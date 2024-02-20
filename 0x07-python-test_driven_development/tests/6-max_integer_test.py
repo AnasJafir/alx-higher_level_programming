57b2b48b71eac9990c2223459233be7f36fcf551
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-20 23:52:08 +0100

    max integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,6 +13,18 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3]),  3)
         self.assertEqual(max_integer([10, 20, 30]), 30)
 
+    def test_max_at_the_beginning(self):
+        """Check if it returns the maximum that is in the beginning of a list"""
+        self.assertEqual(max_integer([10, -10, 7]), 10)
+
+    def test_max_at_the_middle(self):
+        """Check if it can find the maximum number when it's not at the beginning or end"""
+        self.assertEqual(max_integer([-5, 8, -12]), 8)
+
+    def test_one_elm(self):
+        """Check if it returns the max integer of a list of one integer"""
+        self.assertEqual(max_integer([9]), 9)
+
     def test_negatives(self):
         """Check if it raises an exception when given negative numbers."""
         self.assertEqual(max_integer([-5, -4, -3]), -3)
