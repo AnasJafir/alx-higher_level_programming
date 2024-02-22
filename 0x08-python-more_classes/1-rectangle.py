52f635004a436b585e724eaa15478ab3dcf5ec67
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-22 16:09:38 +0100

    getting and setting attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,10 +6,14 @@ class Rectangle():
     """class that defines a rectangle"""
     def __init__(self, width=0, height=0):
         """Instantiation Method"""
-        if type(width) != int or type(height) != int:
+        if type(width) != int:
             raise TypeError("width must be an integer")
-        elif width < 0 or height < 0:
+        elif width < 0:
             raise ValueError("width must be >= 0")
+        elif type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
