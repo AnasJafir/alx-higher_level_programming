50224cb65a2fa498c8e3c6c76331665ccd63a238
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-22 15:56:00 +0100

    getting and setting attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Rectangle Class Module"""
+
+
+class Rectangle():
+    """class that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Instantiation Method"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Method to retrieve value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Method  to set the value of width with validation"""
+        if type(value) != int:
+            raise TypeErrorError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Method to retrieve value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Method to set the value of height with validation"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
