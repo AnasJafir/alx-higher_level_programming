4da567598bfcd501e8601021f7ae838ffe53adb1
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-24 22:28:06 +0100

    Locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module of class that prevents the user from
+dynamically creating new instance attributes
+except if the new instance attribute is called first_name
+"""
+
+
+class LockedClass:
+    """A class to prevent adding new attributes except for 'first_name'."""
+    def __setattr__(self, attr, value):
+        """
+        Checks whether an attempt to add a new attribute is made.
+        If so, it raises an exception.
+        """
+        if attr != "first_name":
+            raise AttributeError(
+                f"'LockedClass' object has no attribute '{attr}'"
+                )
+        else:
+            super().__setattr__(attr, value)
