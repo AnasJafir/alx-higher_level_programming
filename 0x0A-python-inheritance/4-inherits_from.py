18fdb9da44b860736cbbbae2a970371971a2bfd8
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-25 16:37:20 +0100

    Check if an object is an instance of a class that its parent is a specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,5 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class
     Otherwise, False
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    check = isinstance(obj, a_class) and type(obj) != a_class
+    return check
