40513269a2ab012a47b24cda6af7c243e15ea5d4
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-25 16:14:26 +0100

    Checking if an object is an instance of a class or its subclasses

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is king of class Module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if obj inherits from a_class or a superclass"""
+    return isinstance(obj, a_class)
