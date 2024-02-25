11422f2cc69ff76d5e881e0e9f2b3dac1b7631cf
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-25 17:39:37 +0100

    Method to calculate are

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry:
+    """Class that represents the base geometry class."""
+    def area(self):
+        """Calculate the area"""
+        raise Exception("area() is not implemented")
