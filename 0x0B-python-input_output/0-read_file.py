36007f5a9bb335c5207957e7a422ccb41a63496f
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-26 19:08:42 +0100

    read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Method to read a file"""
-    with open(filename, encoding='UTF8') as f:
-        print(f.read())
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
