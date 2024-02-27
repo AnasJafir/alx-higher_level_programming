85853f32cb787f1a039e3d4c0ff2fde7de746501
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-27 23:14:18 +0100

    updating update module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,33 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height
             )
+
+    def update(self, *args, **kwargs):
+        """Method that assigns an argument to each attribute"""
+        if args is not None and len(args) != 0:
+            kwargs = {}
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        for key, value in kwargs.items():
+            if key == "id":
+                if type(value) != int and value is not None:
+                    raise TypeError("id must be an integer")
+                self.id = value
+            if key == "width":
+                self.__width = value
+            if key == "height":
+                self.__height = value
+            if key == "x":
+                self.__x = value
+            if key == "y":
+                self.__y = value
