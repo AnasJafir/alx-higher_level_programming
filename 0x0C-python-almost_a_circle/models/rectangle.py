0d0b7b5af51ffb49c420d974b83f89f4f8d6292f
AnasJafir <dr.anas.jafir@gmail.com>
2024-02-27 22:38:29 +0100

    updating display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,9 +73,13 @@ class Rectangle(Base):
         Method that prints in stdout the Rectangle instance
         with the character #
         """
+        for y in range(self.__y):
+            print()
         for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
-                print('#', end='')
+                print("#", end="")
             print()
 
     def __str__(self):
