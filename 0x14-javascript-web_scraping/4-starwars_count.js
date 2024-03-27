2d7194f4aa0ef231b9409f8055d91cf9fe473ba4
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-26 19:23:55 -0700

    Fetch data from API

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
     const films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
+    let count = 0;
+
+    films.forEach(film => {
+      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
       }
-    }
-    console.log(counter);
+    });
+
+    console.log(count);
   }
 });
