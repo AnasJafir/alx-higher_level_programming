ba23cc8652c43b71d436fb190392b7c41b4473f8
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-26 19:15:58 -0700

    Number of movies where a character is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(film => {
+      if (film.characters.includes('18')) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  }
+});
