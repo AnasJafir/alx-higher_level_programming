7ba353cacffd2d0e5a67d0593ac46c7cd294cbe2
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-26 19:17:55 -0700

    Number of movies where a character is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let counter = 0;
     const films = JSON.parse(body).results;
-    let count = 0;
-
-    films.forEach(film => {
-      if (film.characters.includes('18')) {
-        count++;
+    for (let result = 0; result < films.length; result++) {
+      const characters = films[result].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
+          counter += 1;
+        }
       }
-    });
-
-    console.log(count);
+    }
+    console.log(counter);
   }
 });
