e72f310cccd1b027c0ca87acaf8fab55aa03add5
AnasJafir <dr.anas.jafir@gmail.com>
2025-03-09 15:53:54 +0000

    Refactor last digit evaluation to use modulo operation for comparisons

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-if abs(int(number[-1])) == 0:
-    print(f'Last digit of {number} is {number[-1]} and is 0')
-elif abs(int(number[-1])) > 5:
-    print(f'Last digit of {number} is {number[-1]} and is greater than 5')
+if number % 10 == 0:
+    print(f"Last digit of {number} is {number % 10} and is 0")
+elif number % 10 > 5:
+    print(f"Last digit of {number} is {number % 10} and is greater than 5")
 else:
-    print(f'Last digit of {number} is {number[-1]} and is less than 6 and not 0')
+    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
