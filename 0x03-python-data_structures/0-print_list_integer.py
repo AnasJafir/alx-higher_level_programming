42cdf1f72848488ea3c5543ec422618ebcc10053
AnasJafir <dr.anas.jafir@gmail.com>
2025-03-17 15:08:43 +0000

    Add type check for integers in print_list_integer function

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for x in my_list:
+        if type(x) != int:
+            raise TypeError("my_list must contain only integers")
         print(x)
