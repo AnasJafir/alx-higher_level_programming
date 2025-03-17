9ba3eea06763a39c5ebd915391fc06017db3f6ab
AnasJafir <dr.anas.jafir@gmail.com>
2025-03-17 15:14:34 +0000

    Refactor print_list_integer function to simplify integer printing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for x in my_list:
-        if type(x) != int:
-            raise TypeError("my_list must contain only integers")
-        print(x)
+        print("{:d}".format(x))
