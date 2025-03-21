7c08aed53623691f65370c35ab64236cd14f3ade
AnasJafir <dr.anas.jafir@gmail.com>
2025-03-21 15:07:43 +0000

    Fix multiple_returns function to handle empty sentences and return first character correctly

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
     if length == 0:
-        first_char = 0
-    return length, first_char
+        sentence += "0"
+    return length, sentence[0]
