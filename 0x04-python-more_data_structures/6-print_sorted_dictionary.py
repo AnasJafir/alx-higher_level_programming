dc65272ee6d49e7e6a4341476544364bc2498fcd
AnasJafir <dr.anas.jafir@gmail.com>
2025-07-07 19:21:06 +0100

    Fix formatting in print_sorted_dictionary function for consistent spacing

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def print_sorted_dictionary(a_dictionary):
     new_dictionary = {}
     sorted_keys = sorted(a_dictionary)
     for i in sorted_keys:
         new_dictionary[i] = a_dictionary[i]
     for key in new_dictionary:
-        print(key+':',new_dictionary[key])
+        print(key+':', new_dictionary[key])
