040e09fbb9eb2eb3ad8e82a458e298116f05da64
AnasJafir <dr.anas.jafir@gmail.com>
2025-07-08 17:10:26 +0100

    Add best_score function to find the key with the highest value in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    score = 0
+    if a_dictionary == None or a_dictionary == {}:
+        return None
+    lst = sorted(a_dictionary.values())
+    score = lst[-1]
+    for k in a_dictionary:
+        if a_dictionary[k] == score:
+            return k
