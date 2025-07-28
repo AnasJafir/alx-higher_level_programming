8ca2274bc54ca0fe8a9338f7ff25cdcf71c51486
AnasJafir <dr.anas.jafir@gmail.com>
2025-07-28 21:34:35 +0100

    Refactor safe_print_list_integers to ensure integers are printed correctly and improve output formatting

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,9 +10,21 @@ def safe_print_list_integers(my_list=[], x=0):
         sous_list = int_list[:x]
         nb_prints = 0
         for i in sous_list:
-            print(i, end='')
+            print("{:d}".format(i), end='')
             nb_prints += 1
         print()
         return nb_prints
     except IndexError:
         print("Index out of range")
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
