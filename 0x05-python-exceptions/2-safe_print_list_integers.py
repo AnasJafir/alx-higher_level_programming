7bf12ef5a16e90f8c8005956507358b4ce2e3639
AnasJafir <dr.anas.jafir@gmail.com>
2025-08-11 21:34:23 +0100

    Refactor safe_print_list_integers to simplify logic and improve integer printing

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    int_list = []
-    for i in my_list:
-        if type(i) == int:
-            int_list.append(i)
-        else:
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
             continue
-    try:
-        sous_list = int_list[:x]
-        nb_prints = 0
-        for i in sous_list:
-            print("{:d}".format(i), end='')
-            nb_prints += 1
-        print()
-        return nb_prints
-    except IndexError:
-        print("Index out of range")
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+    print()
+    return count
